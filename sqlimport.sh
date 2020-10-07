d008cdab4b59a11fcf647d486c3e6a8cd75a15a9
Kobayashi Tetsuji <kobatetsu@hotmail.com>
2020-10-07 10:01:54 +0900

    Changing permissions and importing English CSVs

--- sqlimport.sh
+++ sqlimport.sh
@@ -4,4 +4,8 @@ echo -e '.separator "," \n.import ./job_dictionary.csv job_dictionary' | sqlite3
 echo -e '.separator "," \n.import ./industry_list.csv industry_list' | sqlite3 hellowork.sqlite3
 echo -e '.separator "," \n.import ./license_list.csv license_list' | sqlite3 hellowork.sqlite3
 
+echo -e '.separator "," \n.import ./job_dictionary_en.csv job_dictionary_en' | sqlite3 hellowork.sqlite3
+echo -e '.separator "," \n.import ./industry_list_en.csv industry_list_en' | sqlite3 hellowork.sqlite3
+echo -e '.separator "," \n.import ./license_list_en.csv license_list_en' | sqlite3 hellowork.sqlite3
+
 exit
